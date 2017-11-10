9e8af4929b3279e76acc2607fab4e507e9eab89e
Licanueto <licanueto@hotmail.com>
2017-11-10 16:17:56 -0300

    Create libCajas.h

---
+++ libCajas.h
@@ -0,0 +1,19 @@
+#ifndef LIBTPFINAL_H_INCLUDED
+#define LIBTPFINAL_H_INCLUDED
+
+
+//agregarCaja();
+//abrirOcerrarCaja();
+//buscaCaja();
+//agregarClienteACaja();
+//agregarClienteACajaEnTiempoDeterminado();
+//atenderClientes();
+
+
+
+
+
+
+
+
+#endif // LIBTPFINAL_H_INCLUDED
